6a1f9018a399597a27d09ff68e2c51b703a993b4
ikim23 <no-reply>
2020-10-18 13:55:02 +0200

    query by hash

--- svelte/src/api.js
+++ svelte/src/api.js
@@ -1,7 +1,25 @@
-export function search({ userAgent, languages, query }) {
-  const url =
-    'https://rest.opensubtitles.org/search/query-' + encodeURIComponent(query)
-  return Promise.all(
+export const searchByQuery = ({ userAgent, languages, query }) =>
+  search({
+    userAgent,
+    languages,
+    url:
+      'https://rest.opensubtitles.org/search/query-' +
+      encodeURIComponent(query),
+  })
+
+export const searchByHash = ({ userAgent, languages, hash, size }) =>
+  search({
+    userAgent,
+    languages,
+    url:
+      'https://rest.opensubtitles.org/search/moviebytesize-' +
+      size +
+      '/moviehash-' +
+      hash,
+  })
+
+const search = ({ url, userAgent, languages }) =>
+  Promise.all(
     languages.map((lng) =>
       fetch(`${url}/sublanguageid-${lng}`, {
         method: 'GET',
@@ -21,4 +39,3 @@ export function search({ userAgent, languages, query }) {
         downloadUrl: x.SubDownloadLink,
       }))
   )
-}
